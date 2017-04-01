e034d399410d7bcb55fa8ac6a5b92d551699bfd3
Amir Eldor <amir@eize.ninja>
2017-04-01 15:38:51 +0300

    WorkItem can have children and notes pushed

--- src/models/WorkItem.ts
+++ src/models/WorkItem.ts
@@ -5,6 +5,29 @@ export const MAX_TITLE_LENGTH: number = 100
 export const MAX_TEXT_LENGTH: number = 1000
 
 class WorkItem {
+    constructor(title: string = 'no title') {
+        this.title = title
+        this.text = ''
+        this.children = new Array<WorkItem>()
+        this.notes = new Array<WorkItem>()
+        this.tags = new Array<string>()
+        this.users = new Array<User>()
+    }
+
+    get title() { return this.privateTitle.get() }
+    get text() { return this.privateText.get() }
+    get children(): WorkItem[] { return this.privateChildren }
+    get notes(): WorkItem[] { return this.privateNotes }
+    get tags(): string[] { return this.privateTags }
+    get users(): User[] { return this.privateUsers }
+
+    set title(value: string) { this.privateTitle.set(value) }
+    set text(value: string) { this.privateText.set(value) }
+    set children(value: WorkItem[] ) { this.privateChildren = value }
+    set notes(value: WorkItem[]) { this.privateNotes = value }
+    set tags(value: string[]) { this.privateTags = value }
+    set users(value: User[]) { this.privateUsers = value }
+
     private privateTitle = new MaxLengthString('', MAX_TITLE_LENGTH)
     private privateText = new MaxLengthString('', MAX_TEXT_LENGTH)
     private privateChildren: WorkItem[]
@@ -12,21 +35,20 @@ class WorkItem {
     private privateTags: string[]
     private privateUsers: User[]
 
-    get title() { return this.privateTitle.get() }
-    get text() { return this.privateText.get() }
-    get children() { return this.privateChildren }
-    get notes() { return this.privateNotes }
-    get tags() { return this.privateTags }
-
-    set title(value: string) { this.privateTitle.set(value) }
-    set text(value: string) { this.privateText.set(value) }
-
     public setTitle (value: string): boolean {
         return this.privateTitle.set(value)
     }
 
     public setText (value: string): boolean {
         return this.privateText.set(value)
+    }
+
+    public pushChild(item: WorkItem) {
+        this.children.push(item);
+    }
+
+    public pushNote(item: WorkItem) {
+        this.notes.push(item);
     }
 }
 
--- tests/workitem.test.ts
+++ tests/workitem.test.ts
@@ -1,6 +1,6 @@
 import WorkItem from '../src/models/WorkItem'
 
-var workItem = null
+var workItem: WorkItem = null
 
 const sillyText = "Gizmo is the bizmo"
 
@@ -29,5 +29,25 @@ describe("handling text property", () => {
     test("text can be set with a set function", () => {
         expect(workItem.setText(sillyText)).toBeTruthy()
         expect(workItem.text).toBe(sillyText)
+    })
+})
+
+describe("working with children", () => {
+    test("pushing children", () => {
+        workItem.pushChild(new WorkItem("First child"))
+        expect(workItem.children).toHaveLength(1)
+
+        workItem.pushChild(new WorkItem("Second child"))
+        workItem.pushChild(new WorkItem("Third child"))
+        expect(workItem.children).toHaveLength(3)
+    })
+
+    test("pushing notes", () => {
+        workItem.pushNote(new WorkItem("First note"))
+        expect(workItem.notes).toHaveLength(1)
+
+        workItem.pushNote(new WorkItem("Second note"))
+        workItem.pushNote(new WorkItem("Third note"))
+        expect(workItem.notes).toHaveLength(3)
     })
 })
